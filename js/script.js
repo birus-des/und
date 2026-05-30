a8c8c37b760cfda34cbcd7085857133552254d86
birus-des <beerooskun@gmail.com>
2026-05-31 06:28:35 +0700

    Add JavaScript file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Main JavaScript file
+console.log('Welcome to your website!');
+
+// Add your custom scripts here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded');
+});
